8a95dca93cfae53de3dd51b90bdaf69a8d04aec9
crusiris <cruzsirys@gmail.com>
2020-05-19 12:21:42 -0400

    Peticion get para obtener una bebida en particular

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
+import Axios from 'axios';
 
 //Creando el context
 export const ModalContext = createContext();
@@ -7,7 +8,26 @@ export const ModalContext = createContext();
 const ModalProvider = (props)=> {
 
     //State
-    const [idrecipe, saveIdRecipe] = useState(null)
+    const [idrecipe, saveIdRecipe] = useState(null);
+
+    //State
+    const [drink, saveDrink]= useState({});
+
+    //Peticion get al api, cuando se obtenga un id
+    useEffect(()=>{
+        const getRecipe = async ()=>{
+
+            if(!idrecipe) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idrecipe}`;
+            const result = await Axios.get(url);
+            saveDrink(result.data.drinks[0]);
+
+        }
+
+        getRecipe();
+
+    }, [idrecipe])
 
     return(
         <ModalContext.Provider
